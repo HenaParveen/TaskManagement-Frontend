ac887ae5ce8fcae20e522c10bcfa81495d0b2247
HenaParveen <138970020+HenaParveen@users.noreply.github.com>
2024-10-29 22:13:51 +0530

    Initial Setup for the Project like folder and required Library

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { lazy, Suspense } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Loader from "./component/loaders/Loader";
+import Settings from "./pages/settings/Settings";
+import Analytics from "./pages/analytics/Analytics";
+
+const AppLayout = lazy(() => import("./component/layouts/AppLayout"));
+const AuthLayout = lazy(() => import("./component/layouts/AuthLayout"));
+const ViewCard = lazy(() => import("./pages/viewCard/ViewCard"));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={"/"}
+          element={
+            <Suspense fallback={<Loader />}>
+              <AppLayout />
+            </Suspense>
+          }
+        >
+          {/* Child of AppLayout */}
+          <Route index element={<Dashboard />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route
+          path={"/"}
+          element={
+            <Suspense fallback={<Loader />}>
+              <AuthLayout />
+            </Suspense>
+          }
+        >
+          {/* Child Route of AuthLayoutRoute */}
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+        <Route
+          path={"/view/:cardId"}
+          element={
+            <Suspense fallback={<Loader />}>
+              <ViewCard />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
