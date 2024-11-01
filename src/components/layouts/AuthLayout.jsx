cd4e1c17e825d041fdb6721cb8a4f8e5bbbe9246
HenaParveen <138970020+HenaParveen@users.noreply.github.com>
2024-11-01 17:12:49 +0530

    Fixed user profile updation

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Banner from "../banner/Banner";
 function AuthLayout() {
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", width: "100%", height: "100%" }}>
       <Banner />
       <div>
         <Outlet />
